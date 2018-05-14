a656e11c32b82e367411989fc006980042752507
milen-vm <bay_milen@abv.bg>
2018-05-14 23:58:43 +0300

    Added layouts.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,13 @@
+@include('layouts.public.header')
+
+<body>
+
+    <div class="container">
+
+    @yield('content')
+
+    </div>
+
+@include('layouts.public.footer')
+
+</body>
---
+++ resources/views/layouts/public/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Unminified jQ -->
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous"></script>
+<!-- Bootstrap 4 -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+        integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+        crossorigin="anonymous"></script>
+    <link
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+        crossorigin="anonymous">
+<!-- Optional theme -->
+    <link rel="stylesheet" href="">
+    <!-- Compiled css -->
+    <link rel="stylesheet" href="/css/app.css">
+<!-- Compiled js -->
+    <script src="/js/app.js"></script>
+    <title>Photo Album</title>
+    <style>
+    </style>
+</head>
