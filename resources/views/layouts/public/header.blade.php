13b6d656038fc394bf10a6112046d76355d68cc8
milen-vm <bay_milen@abv.bg>
2018-06-03 00:16:48 +0300

    Added jQ 3.0 cdn.

--- resources/views/layouts/public/header.blade.php
+++ resources/views/layouts/public/header.blade.php
@@ -9,6 +9,10 @@
         src="https://code.jquery.com/jquery-3.3.1.js"
         integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
         crossorigin="anonymous"></script>
+    {{--<script--}}
+        {{--src="https://code.jquery.com/jquery-migrate-3.0.1.js"--}}
+        {{--integrity="sha256-VvnF+Zgpd00LL73P2XULYXEn6ROvoFaa/vbfoiFlZZ4="--}}
+        {{--crossorigin="anonymous"></script>--}}
 <!-- Bootstrap 4 -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
         integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
@@ -20,7 +24,7 @@
         crossorigin="anonymous">
 <!-- Optional theme -->
     <link rel="stylesheet" href="">
-    <!-- Compiled css -->
+<!-- Compiled css -->
     <link rel="stylesheet" href="/css/app.css">
 <!-- Compiled js -->
     <script src="/js/app.js"></script>
