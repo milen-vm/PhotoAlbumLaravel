cad396d715bdc5da318d6cf727d64376e4196fb8
milen-vm <bay_milen@abv.bg>
2018-06-09 18:15:02 +0300

    Added Albums table migration.

---
+++ database/migrations/2018_06_04_161300_create_albums_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->boolean('is_private')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id', 'fk_albums_users_user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
